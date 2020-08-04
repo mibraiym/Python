fd40b8d8513bbb1a032ed9e13bfbf644e18b5088
Moldir <moldir.ibraiymova@gmail.com>
2020-08-04 17:54:13 -0400

    formatted comment styling

--- cc-001-calculate-gasoline-production-cost/Calculate the Gasoline Production & Cost.py
+++ cc-001-calculate-gasoline-production-cost/Calculate the Gasoline Production & Cost.py
@@ -1,15 +1,13 @@
 '''Problem Statement
-Write a program that will prompt the user for inputting a floating point number that stands for gallons of gasoline. You will re-print that value along with other information about gasoline and gasoline usage:
-
-Amount of liters
-Amount of barrels of oil required to make this amount of gasoline
-Price in US dollars
-
+Write a program that will prompt the user for inputting a floating point number that stands for gallons of gasoline. 
+You will re-print that value along with other information about gasoline and gasoline usage:
+    Amount of liters
+    Amount of barrels of oil required to make this amount of gasoline
+    Price in US dollars
 Here are some approximate conversion values:
-
-1 gallon is equivalent to 3.7854 liters
-1 barrel of oil approximately produces 19.5 gallons of gas
-God knows what the cost should be, but let’s assume it $0.75/liter
+    1 gallon is equivalent to 3.7854 liters
+    1 barrel of oil approximately produces 19.5 gallons of gas
+    God knows what the cost should be, but let’s assume it $0.75/liter
 '''
 
 gallons_int = input('Enter a number which stands for gallons of gasoline: ')
@@ -22,4 +20,5 @@ cost= liters * 0.75
 print(f"{gallons_fl} gallon(s) = {liters} liter(s).")
 print(f"{barrels} barrel(s) of oil required to produce {gallons_fl} gallon(s) of gas.")
 print(f"It will cost ${cost} dollar(s).")
+
 
