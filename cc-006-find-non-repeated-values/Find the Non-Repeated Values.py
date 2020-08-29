cfbf491cce4982a8fa35741304ad2179cdc623d2
Moldir <moldir.ibraiymova@gmail.com>
2020-08-29 16:47:02 -0400

    Added cc-006

---
+++ cc-006-find-non-repeated-values/Find the Non-Repeated Values.py
@@ -0,0 +1,13 @@
+products = ["Pride and Prejudice", "To Kill a Mockingbird", "The Great Gatsby",\
+"One Hundred Years of Solitude", "Pride and Prejudice", "In Cold Blood", "Wide Sargasso Sea",\
+"One Hundred Years of Solitude", "Brave New World",  "The Great Gatsby", "Brave New World",\
+"I Capture The Castle", "Brave New World", "The Great Gatsby", "The Great Gatsby",\
+"One Hundred Years of Solitude", "Pride and Prejudice"]
+
+uniqueBooks = []
+
+for books in products:
+    if books not in uniqueBooks: 
+        uniqueBooks.append(books)
+
+print(uniqueBooks)
